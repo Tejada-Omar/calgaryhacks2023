615159f75654dcb82faf3fef19cdda407913882e
Yichen Liu <yichen.liu@hotmail.com>
2023-02-18 14:37:50 -0700

    epic commit

---
+++ Pet.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Pet():
+    def __init__(self, health = 100, hunger = 100, fitness = 100, energy = 100):
+        self.health = health
+        self.hunger = hunger
+        self.fitness = fitness
+        self.energy = energy
+
+    def setHunger(self, amount):
+        self.hunger = min(100, amount)
+        
+    def setFitness(self, amount):
+        self.fitness = min(100, amount)
+    
+    def setEnergy(self, amount):
+        self.energy = min(100, amount)
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import pygame
+import sys, pygame
+from Pet import Pet
+pygame.init()
+
+size = width, height = 320, 240
+
+screen = pygame.display.set_mode(size)
+
+cat = pygame.image.load("assets/pets/placeholder.png")
+
+
+def main():
+    FPS = 60
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill((0, 0, 0))
+       #screen.blit(cat, (0, 0))
+        pygame.display.flip()
+        
 
 
 
-class Pet():
-    def __init__(self, health, food, exercise, energy):
-        self.health = health
-        self.food = food
-        self.exercise = exercise
-        self.energy = energy
-
-    def feed(self):
+main()
