d39abb346bf67febc679305b57e7dc1ea68504ad
Omar Tejada <tejada.omar03@gmail.com>
2023-02-18 20:04:34 -0700

    Revert "Implement basic food api and move SpriteSheet"
    This reverts commit 937f5ecebeddcaece2030dff199b971cb7097d88.

--- classes/Pet.py
+++ classes/Pet.py
@@ -1,7 +1,19 @@
-import sys, pygame
-from SpriteSheet import SpriteSheet
-sys.path.append("../")
+import pygame
 from Status import Status, Health, Hunger, Fitness, Energy
+
+
+class SpriteSheet():
+	def __init__(self, image):
+		self.sheet = image
+
+	def get_image(self, frame, width, height, scale, colour):
+		image = pygame.Surface((width, height)).convert_alpha()
+		image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
+		image = pygame.transform.scale(image, (width * scale, height * scale))
+		image.set_colorkey(colour)
+
+		return image
+
 
 class Pet(pygame.sprite.Sprite):
     def __init__(self,spriteSheet, name = None, health = 100, hunger = 100, fitness = 100, energy = 100):
